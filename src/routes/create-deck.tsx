c15a8032c8a3e87634103967110b9f4eec3eb521
MatheusBLopes <matheusblopes10@gmail.com>
2024-05-28 19:41:45 -0300

    feat: update deck page

--- src/routes/create-deck.tsx
+++ src/routes/create-deck.tsx
@@ -12,7 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const FormSchema = z.object({
   name: z.string().min(2, {
@@ -24,61 +24,68 @@ const FormSchema = z.object({
 })
 
 export function CreateDeck() {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-        name: "",
-        description: ""
-        },
-    })
+  const navigate = useNavigate();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      description: ""
+    },
+  })
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     const payload = {
-        "name": data.name,
-        "description": data.description
-      };
-  
+      "name": data.name,
+      "description": data.description
+    };
+
     axios.post(`http://localhost:8000/decks/`, payload)
-        .then(response => {
-            return redirect('/')
-        })
-        .catch(error => {
-            console.error('Error creating deck:', error);
-        })
+      .then(() => {
+        navigate('/')
+      })
+      .catch(error => {
+        console.error('Error creating deck:', error);
+      })
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div className="min-h-screen">
+      <main className="flex-col mx-auto">
+        <div className="container mx-auto px-4 md:px-6 py-12">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Input placeholder="Description" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Description" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
+      </main>
+    </div>
   )
 }
