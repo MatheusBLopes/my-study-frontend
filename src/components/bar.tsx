b00bc8b67a730fcbfc10f0ade03c687413cfd25e
MatheusBLopes <matheusblopes10@gmail.com>
2024-05-29 15:14:59 -0300

    feat: update nav bar

--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -2,12 +2,10 @@ import { Link } from 'react-router-dom';
 
 export function NavBar() {
   return (
-    <header className="flex items-center justify-center h-16 px-4 md:px-6 dark:bg-gray-900 dark:border-gray-800 bg-gray-950 text-gray-50">
-      <nav className="hidden md:flex items-center gap-6">
-        <Link className="text-lg font-medium text-gray-300 hover:underline underline-offset-4" to={"/"}>
-          Flashcards
-        </Link>
-      </nav>
+    <header className="flex h-16 items-center justify-between px-4 md:px-6 dark:bg-gray-900 dark:border-gray-800 bg-gray-950">
+      <Link className="flex items-center gap-2" to="/">
+        <span className="text-lg font-semibold">My Study</span>
+      </Link>
     </header>
   )
 }
